ca363c69a1efcb0d9c0dc27263f553468183813e
Renhao Luo <renhao_luo@hms.harvard.edu>
2024-03-02 09:56:13 -0800

    update readme and singularity files

--- README.md
+++ README.md
@@ -10,5 +10,23 @@ Create a Singularity image containing Rstudio Server and all required R librarie
 
 ## Tutorial
 
+### Option 1
 1. `git clone` this repository to your HPC directory. 
-2. 
+2. Create an online account with [Sylabs](https://sylabs.io/), and generate a personal token. (Usually good for a month)
+3. In HPC, run `singularity remote login`, and enter your username and personal token. 
+4. Run `singularity build --remote rstudio-hpc-v3.sif rstudio-hpc.def` to build the image (this could take a long time)
+5. `sbatch start_rstudio.sh` to submit the job to a computing node. 
+    - Note, modify the `#SBATCH` tags to request different number of CPUs and RAMs.
+6. Check out the `log.txt` for tunnel access and username/password.
+
+### Option 2
+If you have trouble to build the singularity image, you could copy from my repository on HPC. 
+
+1. `git clone` this repository to your HPC directory. 
+2. Inside this repository, `cp /gpfs/home/rluo/rstudio-hpc/rstudio-hpc-v3.sif .`
+3. `sbatch start_rstudio.sh` to submit the job to a computing node. 
+    - Note, modify the `#SBATCH` tags to request different number of CPUs and RAMs.
+4. Check out the `log.txt` for tunnel access and username/password.
+
+## Additional package
+If you need to install additional R library for your Rstudio, try to install the library in your Rstudio first. If got errors, you could modify the `rstudio-hpc.def` file and rebuild the image. 
---
+++ start_rstudio.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+#SBATCH --output="r_studio.log"
+#SBATCH --job-name="rstudio_server"
+#SBATCH --time=12:00:00    # walltime
+#SBATCH --cpus-per-task=8  # number of cores
+#SBATCH --mem-per-cpu=8G   # memory per CPU core
+#SBATCH --error="r_studio.err"   # error log
+
+
+# modified from https://www.rocker-project.org/use/singularity/
+
+export PASSWORD=$(openssl rand -base64 15)
+
+# get unused socket per https://unix.stackexchange.com/a/132524
+# tiny race condition between the python & singularity commands
+readonly PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
+
+echo "ssh -L 8787:${HOSTNAME}:${PORT} ${USER}@login03"
+echo "user: ${USER}"
+echo "password: ${PASSWORD}"
+
+cat >> ${HOME}/rstudio-hpc/log.txt << END
+
+--- `date` ---
+1. SSH tunnel from your workstation using the following command:
+
+   ssh -L 8787:${HOSTNAME}:${PORT} ${USER}@login03
+
+   and point your web browser to http://localhost:8787
+
+2. log in to RStudio Server using the following credentials:
+
+   user: ${USER}
+   password: ${PASSWORD}
+
+When done using RStudio Server, terminate the job by:
+
+1. Exit the RStudio Session ('power' button in the top right corner of the RStudio window)
+2. Issue the following command on the login node:
+
+      qdel -x ${PBS_JOBID}
+END
+
+export TMPDIR="${HOME}/scripps_hpc_rstudio/rstudio-tmp"
+
+# make ssh key for passwordless internode ssh
+# if you already have a public key copy it to .ssh/authorized_keys
+if [ ! -e ${HOME}/.ssh/id_rsa.pub ]
+then
+  cat /dev/zero | ssh-keygen -t rsa -N ""
+  cat ${HOME}/.ssh/id_rsa.pub >> ${HOME}/.ssh/authorized_keys
+  chmod 700 ${HOME}/.ssh; chmod 640 ${HOME}/.ssh/authorized_keys
+fi
+
+# User-installed R packages go into their home directory
+if [ ! -e ${HOME}/.Renviron ]
+then
+  printf '\nNOTE: creating ~/.Renviron file\n\n'
+  echo 'R_LIBS_USER=~/R/%p-library/%v' >> ${HOME}/.Renviron
+  # env vars need to go in the .Renviron file
+fi
+
+# make outfolder (check for errors)
+mkdir -p ${HOME}/rstudio-hpc/output
+
+# create secure-cookie-key (thanks @MboiTui)
+if [ ! -e ${TMPDIR}/tmp/rstudio-server/${USER}_secure-cookie-key ]
+then
+   mkdir -p ${TMPDIR}/tmp/rstudio-server/
+   export UUID=$(python  -c 'import uuid; print(uuid.uuid1())')
+   echo ${UUID} > ${TMPDIR}/tmp/rstudio-server/${USER}_secure-cookie-key
+fi
+
+
+mkdir -p ${TMPDIR}/var/lib
+mkdir -p ${TMPDIR}/var/run
+
+# By default the only host file systems mounted within the container are $HOME, /tmp, /proc, /sys, and /dev.
+# you can use --bind [-B] to bind other file systems
+
+singularity exec \
+   --bind="$TMPDIR/var/lib:/var/lib/rstudio-server" \
+   --bind="$TMPDIR/var/run:/var/run/rstudio-server" \
+   --bind="$TMPDIR/tmp:/tmp" \
+   ${HOME}/rstudio-hpc/rstudio-hpc-v3.sif bash -c "\
+   rserver --www-port ${PORT} --auth-none=0 --auth-pam-helper-path=pam-helper \
+   --auth-timeout-minutes=0 --auth-stay-signed-in-days=30 --server-user ${USER} \
+   --secure-cookie-key-file ${HOME}/tmp/rstudio-server/${USER}_secure-cookie-key \
+   "
